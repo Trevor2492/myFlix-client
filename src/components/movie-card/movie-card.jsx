576fd757aba9b11a3ea33adb4c2dbaf5b1112d95
Trevor Allen <trevor2492@gmail.com>
2021-08-12 22:26:42 -0500

    updated some of the movie-card content to be more readable

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,7 +14,7 @@ export class MovieCard extends React.Component{
 				<Card className="movie-card_card">
 					<Card.Img variant="top" className="card-img" src={movie.ImagePath} />
 					<Card.Body>
-						<Card.Title style={{ fontSize: 25 }}>{movie.Title}</Card.Title>
+						<Card.Title style={{ fontSize: 25, fontWeight: 'bold' }}>{movie.Title}</Card.Title>
 						<Card.Text>{movie.Description.slice(0,75)}...</Card.Text>
 						<Link to={`/movies/${movie._id}`}>
 							<Button className="card-button" variant="primary">Learn More</Button>
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -61,15 +61,15 @@ class MovieView extends React.Component{
 			<Card className="movie-view_card">
 				<Card.Img className="movie-view_image" variant="top" src={movie.ImagePath} />
 				<Card.Body>
-					<Card.Title>{movie.Title}</Card.Title>
+					<Card.Title style={{ fontSize: '25px', fontWeight: 'bold' }}>{movie.Title}</Card.Title>
 					<Card.Text>
 						{movie.Description}
 					</Card.Text>
 					<Card.Text>
-						{movie.Genre.Name}
+						Genre: {movie.Genre.Name}
 					</Card.Text>
 					<Card.Text>
-						{movie.Director.Name}
+						Director: {movie.Director.Name}
 					</Card.Text>
 
 					<div className="movie-view_buttons">
